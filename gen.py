018fd20ac6575cf9b861534b9d1b4d3a1b921fe5
Bluefissure <bluefissure@foxmail.com>
2023-07-03 22:17:29 +0000

    6.x new duties

--- gen.py
+++ gen.py
@@ -2,13 +2,24 @@ import requests
 import re
 import csv
 import json
+from bs4 import BeautifulSoup
+
+questIdSet = {}
 
 def load_web_duties():
     r = requests.get("https://ff14.org/duty/")
-    duties = re.findall(r"\/duty\/(\d+)\.htm", r.text)
-    questIdSet = list(map(lambda x: int(x), duties))
-    print(questIdSet)
-
+    bs = BeautifulSoup(r.text, "html.parser")
+    duty_list = bs.find('ul', {'class': 'duty-list'})
+    for duty in duty_list.children:
+        if not duty or not duty.a:
+            continue
+        href = duty.a.attrs.get('href')
+        m = re.match(r"\/duty\/(\d+)\.htm", href)
+        if not m:
+            continue
+        duty_id = int(m.group(1))
+        questIdSet[duty_id] = duty.text.strip()
+    print(questIdSet.keys())
 
 def load_from_territory():
     r = requests.get("https://raw.githubusercontent.com/thewakingsands/ffxiv-datamining-cn/master/TerritoryType.csv")
@@ -18,7 +29,8 @@ def load_from_territory():
     print("{")
     for row in my_list:
         if row[0].isdigit() and int(row[11]) > 0:
-            print("  {}: {},".format(int(row[0]), int(row[11])))
+            print("  {}: {},\t//{}".format(
+                int(row[0]), int(row[11]), questIdSet.get(int(row[11]), "")))
     print("}")
 
 
